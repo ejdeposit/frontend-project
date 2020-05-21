ab52b0119bb8e2d8206170a60abbcd4aca9e495d
ejdeposit <edeposit@pdx.edu>
2020-05-20 21:12:58 -0700

    figured out how to loop through states for api calls using await iterable

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-//const url = 'https://swapi.dev/api/people/';
-//let baseUrl = 'https://swapi.dev/api/people/?page='; 
-//let outputlist = document.querySelector("ul#results");
-
 function make_graph(chart, points){
     var ctx = document.getElementById('myChart');
 
@@ -28,7 +24,6 @@ function get_state_dailies(st){
     let state= st
     let requesturl= `https://covidtracking.com/api/v1/states/${state}/daily.json`
 
-    let next = null
     fetch(`${requesturl}`)
     .then(response => {
         //console.log('RESPONSE')
@@ -59,8 +54,59 @@ function get_state_dailies(st){
     })
 }
 
+async function make_call(st){
+    let state= st
+    let requesturl= `https://covidtracking.com/api/v1/states/${state}/daily.json`
+    return new Promise(resolve=>{
+        fetch(`${requesturl}`)
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            //create array of point obnjects
+            let points=[]
+            data.forEach(obj => {
+                let newPoint = {}
+                newPoint.x=obj.date
+                newPoint.y=obj.deathIncrease
+                points.push(newPoint)
+            });
+            // filter out anything that isn't plottable number 
+            points = points.filter(point => typeof point.y === 'number')
+            console.log(points);
+            return points;
+        })
+    })
+}
+let get_data_async = async (st) => {
+    let state= st
+    let requesturl= `https://covidtracking.com/api/v1/states/${state}/daily.json`
+    let response = await fetch(requesturl)
+    let data = await response.json()
+    return data
+}
+
+// need to await the result of this function before making graphs
+async function make_calls(states){
+    // list of lists
+    pointss=[]
+
+    //let newPoints = await get_data_async(states[0])
+    //pointss.push(newPoints)
+    for await(let state of states){
+        let newPoints = await get_data_async(state)
+        pointss.push(newPoints)
+    }
+    return pointss
+}
+
+async function make_graphs(states){
+    pointss = await make_calls(states) 
+    console.log(pointss)
+}
+
 console.log('hello world!')
 
-let points = get_state_dailies('CA')
-//console.log(points)
+//let points = get_state_dailies('CA')
+make_graphs(['CA', 'WA'])
 
