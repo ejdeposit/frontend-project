91178d645a0c2e55f58d3419d70a523b734345b6
ejdeposit <edeposit@pdx.edu>
2020-06-01 21:03:33 -0700

    finished cases and deaths per 1000 residents graph options

--- app.js
+++ app.js
@@ -1,4 +1,57 @@
 
+statePopulations = {
+    AL:4903185,
+    AK:731545,
+    AZ:7278717,
+    AR:3017804,
+    CA:39512223,
+    CO:5758736,
+    CT:3565287,
+    DE:973764,
+    FL:21477737,
+    GA:10617423,
+    HI:1415872,
+    ID:1787065,
+    IL:12671821,
+    IN:6732219,
+    IA:3155070,
+    KS:2913314,
+    KY:4467673,
+    LA:4648794,
+    ME:1344212,
+    MD:6045680,
+    MA:6892503,
+    MI:9986857,
+    MN:5639632,
+    MS:2976149,
+    MO:6137428,
+    MT:1068778,
+    NE:1934408,
+    NV:3080156,
+    NH:1359711,
+    NJ:8882190,
+    NM:2096829,
+    NY:19453561,
+    NC:10488084,
+    ND:762062,
+    OH:11689100,
+    OK:3956971,
+    OR:4217737,
+    PA:12801989,
+    RI:1059361,
+    SC:5148714,
+    SD:884659,
+    TN:6829174,
+    TX:28995881,
+    UT:3205958,
+    VT:623989,
+    VA:8535519,
+    WA:7614893,
+    WV:1792147,
+    WI:5822434,
+    WY:578759
+}
+
 //  ------------------------------------------------
 //                   make lien graph for single state
 //                   from single fetch to api 
@@ -149,8 +202,21 @@ async function state_daily_graph(pastCalls, statesInput, outPutId, graphSelectio
     else if(graphSelection === 'avgDeathIncrease'){
         yVariable = 'deathIncrease';
         sevenDayAvg = true;
-        graphTitle = "Seven Day Average in New Deaths"
-    } 
+        graphTitle = "Seven Day Average in New Deaths";
+    }
+    else if(graphSelection ==='deathsPerK'){
+        yVariable = 'deathIncrease';
+        twoWeekCum = true;
+        graphTitle = "New Deaths Per 1000 Residents";
+
+    }
+    else if(graphSelection ==='casesPerK'){
+        yVariable = 'positiveIncrease';
+        twoWeekCum = true;
+        graphTitle = "New cases Per 1000 Residents";
+
+    }
+
     
     
     //figure out which calls where already made
@@ -190,7 +256,9 @@ async function state_daily_graph(pastCalls, statesInput, outPutId, graphSelectio
         dataSubsets = seven_day_avg(dataSubsets)
     }
     
-    //two_week_cumulutive(dataSubsets)
+    if(twoWeekCum){
+        dataSubsets = two_week_cumulutive(dataSubsets)
+    } 
 
     //filter out negative numbers if deats or 
      
@@ -358,8 +426,6 @@ function randColor(){
     return '#' + randColor
 }
 
-
-// something borken here changed if condition for return empty object
 function seven_day_avg(datas){
     console.log('Seven_day_avg()')
     console.log('datas')
@@ -367,9 +433,6 @@ function seven_day_avg(datas){
     let states = Object.keys(datas)
 
     if(Object.keys(datas).length === 0){
-        console.log('no states in data subsets')
-        console.log(datas)
-        console.log(object.keys(datas))
         return {}
     }
 
@@ -380,8 +443,6 @@ function seven_day_avg(datas){
     let yVariable = get_y_variable(datas)
 
     //states data consists of list of objects
-    console.log('data keys')
-    console.log(states)
     states.forEach(state =>{
         let avgData = [];
         let data=datas[state];
@@ -475,13 +536,15 @@ function two_week_cumulutive(datas){
                 accum = accum + day[yVariable]
             });
             let newWeek = {};
-            newWeek[yVariable]= accum;
+            newWeek[yVariable]=1000 * accum/statePopulations[state];
             newWeek['date']=dateStr;
             reducedData[state].push(newWeek)
             console.log(newWeek)
+            console.log(statePopulations[state])
             console.log(week)
         });
     });
+
     //divide data by pop or separate function
     return reducedData
 }
